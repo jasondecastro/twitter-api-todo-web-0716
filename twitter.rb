6c162d4058310a79853ce767acb43cc0de6efc2b
Jason Decastro <jasonrdecastro@gmail.com>
2016-07-27 16:35:04 -0400

    finished all four methods to scrape twitter user

--- twitter.rb
+++ twitter.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'yaml'
+require 'pry'
 
 class TwitterApi
   attr_reader :client
@@ -15,31 +16,69 @@ class TwitterApi
   end
 
 
-  def most_recent_friend
+  def most_recent_friend(username)
     #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    client.friends(username).attrs[:users][0][:screen_name]
   end
 
   def find_user_for(username)
     #find the twitter gem method that returns the correct user, given a username
+    client.user(username).attrs[:name]
   end
 
   def find_followers_for(username)
     #find the twitter gem method that returns the follows of a given user
+    counter = 0
+    result = client.followers(username)
+    followers = []
+    20.times do 
+      followers << result.attrs[:users][counter][:screen_name]
+      counter += 1
+    end
+    followers
   end
 
-  def homepage_timeline
+  def homepage_timeline(username)
     #find the twitter gem method that retreives the tweets from a user's timeline.
+    tweets = []
+    client.user_timeline(username).each do |x|
+      tweets << x.text
+    end
+    tweets.each {|x| puts x + "\n\n\n"}
+  end
+
+  def trump(username)
+    #find the twitter gem method that retreives the tweets from a user's timeline.
+    tweets = []
+    client.user_timeline(username).each do |x|
+      tweets << x.text
+    end
+    
   end
   
 end
 
 #Bonus: 
-
 # uncomment out the following and read the bonus instructions.
 # remember to comment out the WebMock line of your spec_helper, as the instructions dictate.
 
-# tweet_client = TwitterApi.new
-# puts tweet_client.most_recent_friend
-# puts tweet_client.find_user_for("USERNAME HERE")
-# puts tweet_client.find_followers_for("USERNAME HERE")
-# puts tweet_client.homepage_timeline
+tweet_client = TwitterApi.new
+puts "Below is the first method\n"
+puts "-" * 40
+puts tweet_client.most_recent_friend("realdonaldtrump")
+puts ""
+puts ""
+puts "Below is the second method\n"
+puts "-" * 40
+puts tweet_client.find_user_for("realdonaldtrump")
+puts ""
+puts ""
+puts "Below is the third method\n"
+puts "-" * 40
+puts tweet_client.find_followers_for("realdonaldtrump")
+puts ""
+puts ""
+puts "Below is the fourth method\n"
+puts "-" * 40
+puts tweet_client.homepage_timeline("realdonaldtrump")
+
